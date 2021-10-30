891e0f5660d8eb557ea19bbc23dc01d330b985e3
Christopher Chan-Nui <christopher.chan-nui@intel.com>
2021-10-30 04:24:44 -0500

    Initial Add

---
+++ {{cookiecutter.project_name}}/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * {{ cookiecutter.project_name }}
+ * {{ cookiecutter.full_name }} <{{ cookiecutter.email }}>
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ {{cookiecutter.project_name}}/tests/test1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by {{cookiecutter.full_name}}
+//
+
+#include "catch.hpp"
+
+TEST_CASE("Sample tests") {
+    REQUIRE(1 == 1);
+}
